ca69754690328af76acaec769ae7a0dd9724c164
reinhardwafula <rehynsanders@gmail.com>
2021-12-02 07:48:31 +0300

    Overriden onBackPressed in DashboardActivity

--- app/src/main/java/com/example/shoeshop/DashboardActivity.kt
+++ app/src/main/java/com/example/shoeshop/DashboardActivity.kt
@@ -2,14 +2,14 @@ package com.example.shoeshop
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
+//import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.shoeshop.databinding.ActivityDashboardBinding
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : BaseActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
 
@@ -31,5 +31,8 @@ class DashboardActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+    override fun onBackPressed() {
+        doubleBackToExit()
     }
 }
